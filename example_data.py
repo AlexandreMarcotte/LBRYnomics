993b5e464c1285d8d2868278200eb824803daba8
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-01-12 12:50:54 +1300

    Renamed a variable

--- example_data.py
+++ example_data.py
@@ -24,8 +24,8 @@ expected_num_tips = lambda_tips*duration
 num_tips = rng.poisson(expected_num_tips)
 
 # Uniform distribution for times given number 
-ts = publish_time + duration*rng.rand(num_tips)
-ts = np.sort(ts)
+times = publish_time + duration*rng.rand(num_tips)
+times = np.sort(times)
 
 # Amounts of tips
 amounts = mu_tips*np.exp(sig_log_tips*rng.randn(num_tips))
@@ -37,15 +37,17 @@ f.write("publish_time: " + str(publish_time) + "\n")
 f.write("current_time: " + str(current_time) + "\n")
 f.write("times:\n")
 for i in range(num_tips):
-    f.write("    - " + str(ts[i]) + "\n")
+    f.write("    - " + str(times[i]) + "\n")
 f.write("amounts:\n")
 for i in range(num_tips):
     f.write("    - " + str(amounts[i]) + "\n")
 f.close()
 
-# Plot tips
-plt.bar(ts, amounts, align="center", width=0.3)
-plt.xlabel("Time (daamounts)")
+## Plot tips
+#def plot_peaks():
+
+plt.bar(times, amounts, align="center", width=0.3)
+plt.xlabel("Time (days)")
 plt.ylabel("Amount (LBC)")
 plt.show()
 
