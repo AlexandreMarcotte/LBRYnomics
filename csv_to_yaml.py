8c3d1150c47aec5f98de2409659c06bfcd35e0fd
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-03-10 19:42:10 +1300

    Finished conversion script

--- csv_to_yaml.py
+++ csv_to_yaml.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 # Load exported CSV file
@@ -11,10 +12,29 @@ subset = data.loc[tips, :]
 subset = subset.iloc[::-1, :]
 
 # Extract times, converting units to days
-ts = subset["timestamp"]/86400.0
-amounts = subset["amount"]
+ts = np.array(subset["timestamp"]/86400.0)
+amounts = np.array(subset["amount"])
 
-import matplotlib.pyplot as plt
-plt.plot(ts, amounts, "o-")
-plt.show()
+# Open output file
+f = open("lbry-transactions-history.yaml", "w")
+
+# t_start and t_end in the model were supposed to be the publication time
+# of a single claim and the current time respectively, so this isn't right,
+# because this script is only looking at tip times.
+t_start = ts.min()
+t_end   = ts.max()
+
+f.write("# YAML data about tips received.\n---\n")
+f.write("t_start: " + str(t_start) + "\n")
+f.write("t_end: "   + str(t_end)   + "\n")
+
+f.write("times:\n")
+for i in range(len(ts)):
+    f.write("    - " + str(ts[i]) + "\n")
+
+f.write("amounts:\n")
+for i in range(len(ts)):
+    f.write("    - " + str(amounts[i]) + "\n")
+
+f.close()
 
