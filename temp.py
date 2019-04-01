8fc39f5e1b0ce911fac9b44b753ebefaf3b9f149
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-04-01 19:11:42 +1300

    YAML saver

--- temp.py
+++ temp.py
@@ -1,9 +1,10 @@
 from ira import *
+import time
 
 # Initialise one of these things
 lbry = lbryRPC()
 
-def get_data(channel_name, page_size=10):
+def get_data(channel_name, page_size=100):
     """
     Get all the data from a given channel and return it in a nice
     format (list of dicts).
@@ -36,6 +37,8 @@ def get_data(channel_name, page_size=10):
             support_heights = []
             support_amounts = []
 
+            print("Processing tips from claim", end="", flush=True)
+
             for support in supports:
                 try:
                     support_tx = lbry.lbry_call("transaction_show",
@@ -43,14 +46,12 @@ def get_data(channel_name, page_size=10):
                     support_heights.append(support_tx[0]["height"])
                     support_amounts.append(float(support["amount"]))
                 except:
-                    print("Error encountered. Trying again in 5 seconds.")
-                    import time
-                    time.sleep(5.0)
-
+                    time.sleep(3.0)
                     support_tx = lbry.lbry_call("transaction_show",
                                             {"txid": support["txid"]})
                     support_heights.append(support_tx[0]["height"])
                     support_amounts.append(float(support["amount"]))
+                print(".", end="", flush=True)
 
             this_claim_data["claim_height"] = height
             this_claim_data["support_heights"] = support_heights
@@ -58,12 +59,11 @@ def get_data(channel_name, page_size=10):
             this_claim_data["num_supports"] = len(support_heights)
 
             data.append(this_claim_data)
-            print("Processed claim {num}.".format(num=len(data)))
+            print(".\nProcessed claim {num}.".format(num=len(data)))
             print(this_claim_data)
-            print("\n\n\n\n", flush=True)
+            print("\n", flush=True)
 
     return data
-
 
 
 def write_flattened(data, filename="data.yaml"):
@@ -71,12 +71,38 @@ def write_flattened(data, filename="data.yaml"):
     Input: a data list as output by get_data().
     Output: a YAML dataset in the current format.
     """
+    # Extract times of claims, to get a start and time
+    # (violating model assumptions slightly - end time should be current block
+    # height, model should be modified so start time is first claim)
+    times = []
+    for claim in data:
+        times.append(claim["claim_height"])
+
     f = open(filename, "w")
-    f.write("---")
+    f.write("---\n")
+    f.write("t_start: " + str(min(times)) + "\n")
+    t_end = max(times)
+
+    # Get tip times and amounts
+    times = []
+    amounts = []
+    for claim in data:
+        times   = times + claim["support_heights"]
+        amounts = amounts + claim["support_amounts"]
+    t_end = max([t_end, max(times)])
+    f.write("t_end: " + str(t_end) + "\n")
+
+    f.write("times:\n")
+    for time in times:
+        f.write("    - " + str(time) + "\n")
+    f.write("amounts:\n")
+    for amount in amounts:
+        f.write("    - " + str(amount) + "\n")
+
     f.close()
 
 
 if __name__ == "__main__":
-    x = get_data("@TheCryptoLark")
+    data = get_data("@BrendonBrewer")
     write_flattened(data)
 
