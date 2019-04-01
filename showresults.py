af0f01c556fe706c6f6f5c273e620e55d9738413
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-04-02 12:20:59 +1300

    Do tip rate

--- showresults.py
+++ showresults.py
@@ -1,9 +1,15 @@
 import dnest4.classic as dn4
 import matplotlib.pyplot as plt
 import numpy as np
+import yaml
 
 # Postprocess DNest4 output
 dn4.postprocess()
+
+# Load the data
+f = open("data.yaml")
+data = yaml.load(f, Loader=yaml.SafeLoader)
+f.close()
 
 # Plot the forecast
 posterior_sample = dn4.my_loadtxt("posterior_sample.txt")
@@ -13,9 +19,12 @@ n = len(amount)
 indices = [ int(0.1*n), int(0.5*n), int(0.9*n) ]
 quantiles = amount[indices]
 
-print("Future tips (10%, 50%, 90% quantiles):", np.round(quantiles, 2))
+tip_rate = posterior_sample[:,-1]/(data["t_end"] - data["t_start"])*1000.0
 
-plt.hist(posterior_sample[:,-1], 100)
-plt.xlabel("Future tips")
+print("Future tip rate per 1000 blocks (10%, 50%, 90% quantiles): ",
+      np.round(quantiles, 2), "LBC.")
+
+plt.hist(tip_rate, 100)
+plt.xlabel("Future tip rate (LBC per kiloblock)")
 plt.show()
 
