9316c9240ed1d18ab36ead016046105f5ce32897
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-04-06 13:00:11 +1300

    Batch stuff (very much alpha)

---
+++ batch.py
@@ -0,0 +1,30 @@
+import fetch_data
+import showresults
+import subprocess
+
+# List of channels to make forecasts for
+channels = ["@Lunduke", "@NaomiBrockwell", "@TheLinuxGamer",
+            "@TheCryptoLark", "@CryptoCandor", "@mikenayna",
+            "@no1marmadukefan", "@TechFox", "@GaminGHD",
+            "@MinutePhysics", "@GamesGlitches",
+            "@upside", "@OpenSourceGames", "@paulvanderklay",
+            "@JordanBPeterson", ]
+
+f = open("forecasts.csv", "w")
+f.write("channel_name,forecast_lower,forecast_medium,forecast_upper\n")
+f.flush()
+
+for channel in channels:
+    data = fetch_data.get_data(channel)
+    fetch_data.write_flattened(data)
+    subprocess.call(["./main", "-t 10"])
+    quantiles = showresults.postprocess()
+
+    f.write(channel + ",")
+    f.write(str(quantiles[0]) + ",")
+    f.write(str(quantiles[1]) + ",")
+    f.write(str(quantiles[2]) + "\n")
+    f.flush()
+
+f.close()
+
--- showresults.py
+++ showresults.py
@@ -3,32 +3,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 import yaml
 
-# Postprocess DNest4 output
-dn4.postprocess()
+def postprocess():
 
-# Load the data
-f = open("data.yaml")
-data = yaml.load(f, Loader=yaml.SafeLoader)
-f.close()
+    # Postprocess DNest4 output
+    dn4.postprocess(plot=False)
 
-# Plot the forecast
-posterior_sample = dn4.my_loadtxt("posterior_sample.txt")
-amount = posterior_sample[:,-1]
-amount = np.sort(amount)
-n = len(amount)
-indices = [ int(0.1*n), int(0.5*n), int(0.9*n) ]
-future_tips = amount
-quantiles = future_tips[indices]
+    # Load the data
+    f = open("data.yaml")
+    data = yaml.load(f, Loader=yaml.SafeLoader)
+    f.close()
 
-print("")
-print("Assuming things keep rolling along more or less as they have been, I")
-print("predict, with 90% probability, that you'll receive between")
-print(np.round(quantiles[0], 2), "and", np.round(quantiles[2], 2), "LBC",
-      end=" ")
-print("over the next month.")
+    # Plot the forecast
+    posterior_sample = dn4.my_loadtxt("posterior_sample.txt")
+    amount = posterior_sample[:,-1]
+    amount = np.sort(amount)
+    n = len(amount)
+    indices = [ int(0.1*n), int(0.5*n), int(0.9*n) ]
+    future_tips = amount
+    quantiles = future_tips[indices]
 
-plt.hist(future_tips, 500, density=True)
-plt.xlabel("Future tips over next month (LBC)")
-plt.ylabel("Probability Density")
-plt.show()
+    print("")
+    print("Assuming things keep rolling along more or less as they have been, I")
+    print("predict, with 90% probability, that you'll receive between")
+    print(np.round(quantiles[0], 2), "and", np.round(quantiles[2], 2), "LBC",
+          end=" ")
+    print("over the next month.")
+
+    # plt.hist(future_tips, 500, density=True)
+    # plt.xlabel("Future tips over next month (LBC)")
+    # plt.ylabel("Probability Density")
+    # plt.show()
+
+    return quantiles
+
+
+if __name__ == "__main__":
+    postprocess()
 
