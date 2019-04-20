17b2aa5c18283b53f0fb685d2852a71e20f75a9d
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-04-20 19:53:46 +1200

    Extract times and amounts...this seems to include withdrawn tips!

--- chainquery.py
+++ chainquery.py
@@ -9,15 +9,33 @@ channel_name = "@BrendonBrewer"
 # Get the channel's claim_id by doing a lbrynet resolve
 from fetch_data import lbry
 result = lbry.lbry_call("resolve", {"urls": [channel_name]})
-claim_id = result[0][channel_name]["certificate"]["claim_id"]
+channel_claim_id = result[0][channel_name]["certificate"]["claim_id"]
 
 # The SQL query to perform
-query = "SELECT name from claim WHERE publisher_id = '" + claim_id + "';"
+query = "SELECT support_amount amount, transaction.transaction_time time FROM\
+            claim\
+            INNER JOIN support\
+            ON claim.claim_id = support.supported_claim_id\
+            INNER JOIN transaction\
+            ON support.transaction_hash_id = transaction.hash\
+            WHERE publisher_id = '" + channel_claim_id + "';"
 
+# Get all claims from the channel
 request = requests.get("https://chainquery.lbry.com/api/sql?query=" + query)
 the_dict = request.json()
 
 if the_dict["success"]:
-    pprint(the_dict)
 
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    amounts = np.empty(len(the_dict["data"]))
+    times   = np.empty(len(the_dict["data"]))
+    for i in range(len(times)):
+        amounts[i] = float(the_dict["data"][i]["amount"])
+        times[i]   = float(the_dict["data"][i]["time"])
+
+    plt.plot(times, amounts, "o")
+    plt.ylim(bottom=0.0)
+    plt.show()
 
