740ac801db19ec3ab1765f1c2db89e55d2be4103
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-04-20 18:40:11 +1200

    Used SQL to fetch published names by me

--- chainquery.py
+++ chainquery.py
@@ -1,13 +1,23 @@
-# Trying to use chainquery instead of ira
+# Trying to use chainquery
 from pprint import pprint
 import requests
 import urllib.request
 
+# Channel name
+channel_name = "@BrendonBrewer"
+
+# Get the channel's claim_id by doing a lbrynet resolve
+from fetch_data import lbry
+result = lbry.lbry_call("resolve", {"urls": [channel_name]})
+claim_id = result[0][channel_name]["certificate"]["claim_id"]
+
 # The SQL query to perform
-query = "SELECT * from claim WHERE name=\"lbry\""
+query = "SELECT name from claim WHERE publisher_id = '" + claim_id + "';"
 
 request = requests.get("https://chainquery.lbry.com/api/sql?query=" + query)
-json = request.json()
+the_dict = request.json()
 
-pprint(json)
+if the_dict["success"]:
+    pprint(the_dict)
+
 
