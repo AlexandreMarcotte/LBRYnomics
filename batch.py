345261998f0d634bfadc3984af24814582816ee0
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-04-21 07:06:25 +1200

    metal guitar dude

--- batch.py
+++ batch.py
@@ -28,7 +28,7 @@ channels = sorted(["@Lunduke", "@NaomiBrockwell", "@TheLinuxGamer",
                    "@Archaeosoup", "@eevblog", "@cleverly", "@FEEOrg",
                    "@adrianbonillap#2f10475ee7090d2030ee2dd17eb4b39bc9437970",
                    "@ronnietucker#2b01f813801407ba1f6ece40ddf94db9ffb04481",
-                   "@UCurU6GLM4ggcLtWWAOTzlYw"],
+                   "@UCurU6GLM4ggcLtWWAOTzlYw", "@roshansuares"],
                     key=lambda s: s.lower())
 
 f = open("forecasts.csv", "w")
