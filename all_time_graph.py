ff91d54252dfb56f5cf9f58e5b0acff3046e0e24
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-06-29 10:41:42 +1200

    Put it in a function

--- all_time_graph.py
+++ all_time_graph.py
@@ -6,79 +6,92 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sqlite3
 
-# Open the DB
-db_file = "/home/brewer/local/lbry-sdk/lbry/lbryum-data/claims.db"
-conn = sqlite3.connect(db_file)
-c = conn.cursor()
+def make_graph(mode):
+    """
+    mode must be "claims" or "channels"
+    """
+    if mode != "claims" and mode != "channels":
+        return
 
-# List for results
-times = []
+    # Open the DB
+    db_file = "/home/brewer/local/lbry-sdk/lbry/lbryum-data/claims.db"
+    conn = sqlite3.connect(db_file)
+    c = conn.cursor()
 
-# Iterate over query results
-i = 0
-for t in c.execute("SELECT creation_timestamp FROM claim\
-                              WHERE claim_type <> 2;"): # Exclude channel claims
-    times.append(t)
-    i = i + 1
-    print(i)
+    # List for results
+    times = []
 
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
-conn.close()
+    # Iterate over query results
+    i = 0
+    for t in c.execute("SELECT creation_timestamp FROM claim\
+                                  WHERE claim_type <> 2;"): # Exclude channel claims
+        times.append(t)
+        i = i + 1
+        print(i)
 
-# Sort the times and convert to a numpy array
-times = np.sort(np.array(times))
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    conn.close()
 
-plt.rcParams["font.family"] = "serif"
-plt.rcParams["font.size"] = 14
-plt.rc("text", usetex=True)
+    # Sort the times and convert to a numpy array
+    times = np.sort(np.array(times))
 
-plt.figure(figsize=(15, 11))
+    plt.rcParams["font.family"] = "serif"
+    plt.rcParams["font.size"] = 14
+    plt.rc("text", usetex=True)
 
-plt.subplot(2, 1, 1)
-times_in_days = (times - 1483228800)/86400.0
-days = times_in_days.astype("int64")
-plt.plot(times_in_days,
-            np.arange(len(times)), "k-", linewidth=1.5)
-plt.ylabel("Cumulative number of claims")
-plt.title("Total number of claims = {n}.".format(n=len(times)))
-plt.xlim([0.0, days.max() + 1])
-plt.ylim(bottom=-100)
-plt.gca().grid(True)
-plt.gca().tick_params(labelright=True)
+    plt.figure(figsize=(15, 11))
 
-import time
-plt.subplot(2, 1, 2)
-bin_width = 1.0
-bins = np.arange(0, np.max(days)+2) - 0.5*bin_width # Bin edges including right edge of last bin
-counts = plt.hist(days, bins, alpha=0.5, color="g", label="Raw",
-                    width=bin_width, align="mid")[0]
+    plt.subplot(2, 1, 1)
+    times_in_days = (times - 1483228800)/86400.0
+    days = times_in_days.astype("int64")
+    plt.plot(times_in_days,
+                np.arange(len(times)), "k-", linewidth=1.5)
+    plt.ylabel("Cumulative number of claims")
+    plt.title("Total number of claims = {n}.".format(n=len(times)))
+    plt.xlim([0.0, days.max() + 1])
+    plt.ylim(bottom=-100)
+    plt.gca().grid(True)
+    plt.gca().tick_params(labelright=True)
 
-# Compute 10-day moving average
-moving_average = np.zeros(len(bins)-1)
-for i in range(len(moving_average)):
-    subset = counts[0:(i+1)]
-    if len(subset) >= 10:
-        subset = subset[-10:]
-    moving_average[i] = np.mean(subset)
-plt.plot(bins[0:-2] + 0.5*bin_width, moving_average[0:-1], "k-",
-            label="10-day moving average", linewidth=1.5)
-#        plt.gca().set_yscale("log")
-plt.xlim([0.0, days.max() + 1])
-plt.xlabel("Time (days since 2017-01-01)")
-plt.ylabel("New claims added each day")
-subset = counts[-31:-1]
-plt.title("Recent average rate (last 30 days) = {n} claims per day.".\
-            format(n=int(np.sum(time.time() - times <= 30.0*86400.0)/30.0)))
-plt.gca().grid(True)
-plt.gca().tick_params(labelright=True)
-#        plt.gca().set_yticks([1.0, 10.0, 100.0, 1000.0, 10000.0])
-#        plt.gca().set_yticklabels(["1", "10", "100", "1000", "10000"])
-plt.legend()
+    import time
+    plt.subplot(2, 1, 2)
+    bin_width = 1.0
 
-plt.savefig("claims.svg", bbox_inches="tight")
-import os
-os.system("cp claims.svg /keybase/public/brendonbrewer/lbry-social")
-print("Figure saved to claims.svg.")
-plt.show()
+    # Bin edges including right edge of last bin
+    bins = np.arange(0, np.max(days)+2) - 0.5*bin_width
+    counts = plt.hist(days, bins, alpha=0.5, color="g", label="Raw",
+                        width=bin_width, align="mid")[0]
+
+    # Compute 10-day moving average
+    moving_average = np.zeros(len(bins)-1)
+    for i in range(len(moving_average)):
+        subset = counts[0:(i+1)]
+        if len(subset) >= 10:
+            subset = subset[-10:]
+        moving_average[i] = np.mean(subset)
+    plt.plot(bins[0:-2] + 0.5*bin_width, moving_average[0:-1], "k-",
+                label="10-day moving average", linewidth=1.5)
+    #        plt.gca().set_yscale("log")
+    plt.xlim([0.0, days.max() + 1])
+    plt.xlabel("Time (days since 2017-01-01)")
+    plt.ylabel("New claims added each day")
+    subset = counts[-31:-1]
+    plt.title("Recent average rate (last 30 days) = {n} claims per day.".\
+                format(n=int(np.sum(time.time() - times <= 30.0*86400.0)/30.0)))
+    plt.gca().grid(True)
+    plt.gca().tick_params(labelright=True)
+    #        plt.gca().set_yticks([1.0, 10.0, 100.0, 1000.0, 10000.0])
+    #        plt.gca().set_yticklabels(["1", "10", "100", "1000", "10000"])
+    plt.legend()
+
+    plt.savefig("claims.svg", bbox_inches="tight")
+    import os
+    os.system("cp claims.svg /keybase/public/brendonbrewer/lbry-social")
+    print("Figure saved to claims.svg.")
+    plt.show()
+
+if __name__ == "__main__":
+    make_graph("claims")
+
 
