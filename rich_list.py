0fa52758925c559caa02d2961f9f38e30fb56e65
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-07-12 14:51:40 +1200

    Top 100

--- rich_list.py
+++ rich_list.py
@@ -4,7 +4,8 @@ import requests
 # NB: The use of the claim_address and the address_list from the output
 # table is to try to only capture tips (not other supports). This also
 # will not capture tips sent to the channel itself.
-query = "SELECT claim.name, claim.id, total_lbc from (SELECT t.publisher_id, SUM(t.amount) total_lbc from (SELECT name, publisher_id, support.support_amount amount\
+query = "SELECT name, almost.* FROM\
+            (SELECT t.publisher_id, SUM(t.amount) total_lbc from (SELECT name, publisher_id, support.support_amount amount\
             FROM claim\
             INNER JOIN support ON support.supported_claim_id = claim.claim_id\
             INNER JOIN transaction ON support.transaction_hash_id = transaction.hash\
@@ -13,13 +14,16 @@ query = "SELECT claim.name, claim.id, total_lbc from (SELECT t.publisher_id, SUM
             GROUP BY support.id, support.support_amount, support.created_at) as t\
                 GROUP BY publisher_id\
                 ORDER BY total_lbc DESC) almost\
-            INNER JOIN claim\
-            ON almost.publisher_id = claim.id"
+            INNER JOIN claim on claim.claim_id = almost.publisher_id"
 
 request = requests.get("https://chainquery.lbry.com/api/sql?query=" + query)
 the_dict = request.json()
-print(the_dict)
 
+k = 0
 for row in the_dict["data"]:
-    if row["id"] is not None:
-        print(str(row["name"]) + "#" + str(row["id"]) + "," + str(row["total_lbc"]))
+    if row["publisher_id"] is not None:
+        print(str(row["name"]) + "#" + str(row["publisher_id"]) + "," + str(row["total_lbc"]))
+        k += 1
+    if k >= 100:
+        break
+
