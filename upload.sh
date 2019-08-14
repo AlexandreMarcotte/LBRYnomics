150f0e0940d8e2548a0275c8fa6761d6187167e0
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-08-15 11:07:23 +1200

    new upload.sh

--- upload.sh
+++ upload.sh
@@ -1,13 +1,4 @@
-#!/usr/bin/expect
-spawn sftp ...
-expect "password:"
-send ...
-expect "sftp>"
-send "cd public_html/lbrynomics\n"
-expect "sftp>"
-send "put *.json\n"
-expect "sftp>"
-send "put *.svg\n"
-expect "sftp>"
-send "exit\n"
-interact
+#!/bin/bash
+sshpass -p "password" scp -P 21098 *.json user@host:/path/public_html/lbrynomics/
+sshpass -p "password" scp -P 21098 *.svg user@host:/path/public_html/lbrynomics/
+
