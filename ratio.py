3d0e644bf92f731e33061999c12a2d1eb6dce6e6
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-09-15 12:57:34 +1200

    report success or failure

--- ratio.py
+++ ratio.py
@@ -5,11 +5,15 @@ directory = "/home/brewer/.local/share/lbry/lbrynet"
 if directory[-1] == "/":
     directory = directory[0:-1]
 
-suffices = ["", ".1", ".2", ".3", ".4", ".5"]
+# Filename endings
+suffices = [""] + ["." + str(i) for i in range(10)]
+
 # Blob counts
 up = 0
 down = 0
 
+# Flag that becomes true ones a file has been successfully opened
+success = False
 for suffix in suffices:
     filename = directory + "/lbrynet.log" + suffix
 
@@ -22,10 +26,14 @@ for suffix in suffices:
             if "lbry.blob_exchange.client:159: downloaded" in line:
                 down += 1
         f.close()
+
+        success = True
     except:
         pass
 
-print("Downloaded {down} blobs.".format(down=down))
-print("Uploaded {up} blobs.".format(up=up))
-print("Ratio = {ratio}".format(ratio=up/down))
+print("Success = {success}.".format(success=success))
+if success:
+    print("Downloaded {down} blobs.".format(down=down))
+    print("Uploaded {up} blobs.".format(up=up))
+    print("Ratio = {ratio}.".format(ratio=up/down))
 
