68a59c5958179db20585e296c907775dd4558218
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-09-17 09:46:56 +1200

    Weekly update

--- run_subscriber_stats.py
+++ run_subscriber_stats.py
@@ -1,17 +1,26 @@
 from fetch_data import *
 import json
+import numpy
 import time
 
 # Needs an initial JSON file to bootstrap from
+
+hour = 3600.0
+day = 24*hour
+week = 7*day
 
 while True:
     f = open("subscriber_counts.json")
     t = json.load(f)["unix_time"]
     f.close()
 
-    if time.time() - t >= 7*86400.0:
+    gap = time.time() - t
+    print("It's been {d} days since the last update."\
+                .format(d=np.round(gap/day, 4)))
+
+    if time.time() - t >= week:
         subscriber_counts("") # <- Put auth token there as a string
 
     # Check about once an hour
-    time.sleep(3600.0)
+    time.sleep(hour)
 
