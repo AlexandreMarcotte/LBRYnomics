d08d40ae096eb4cea37bdd23a00bea8985e596a7
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-11-05 11:00:37 +1300

    Publish the RSS file

--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sshpass -p "password" scp -P 21098 *.json user@host:/path/public_html/lbrynomics/
 sshpass -p "password" scp -P 21098 *.svg user@host:/path/public_html/lbrynomics/
+sshpass -p "password" scp -P 21098 rss.xml user@host:/path/public_html/lbrynomics/
 
