333fad1d19f32aa1d39b96ab6e95fccb1cdc4747
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-11-13 18:50:19 +1300

    Initial delay

--- booster.py
+++ booster.py
@@ -9,11 +9,11 @@ import time
 # Full path to the lbrynet binary
 lbrynet_bin = "/opt/LBRY/resources/static/daemon/lbrynet"
 
-def daemon_command(command):
+def daemon_command(command, message="Calling lbrynet daemon..."):
     """
     Run a daemon command and return its output.
     """
-    print("Calling lbrynet daemon...", end="", flush=True)
+    print(message, end="", flush=True)
     command = lbrynet_bin + " " + command
     parts = command.split(" ")
     output = subprocess.run(parts, capture_output=True)
@@ -39,6 +39,9 @@ amount = float(input("LBC amount of support: "))
 # How many times?
 reps = int(input("Number of repetitions: "))
 
+# Any initial delay?
+delay = float(input("Delay before first support, in hours (e.g., 0, 1, 2, 3, ...): "))*3600.0
+
 # Check available balance
 balance = float(daemon_command("account balance")["available"])
 if amount*reps > balance:
@@ -46,6 +49,7 @@ if amount*reps > balance:
     exit()
 
 # Now do the supports
+time.sleep(delay)
 for i in range(reps):
     daemon_command("support create --claim_id=" + claim_id \
                         + " --amount=" + str(float(amount)))
